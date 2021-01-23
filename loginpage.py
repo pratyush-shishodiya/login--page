1b74bc7c8c39a4627a15ecefb36194e4203e07f3
pratyush-shishodiya <76863772+pratyush-shishodiya@users.noreply.github.com>
2021-01-23 20:42:50 +0530

    Update loginpage.py

--- loginpage.py
+++ loginpage.py
@@ -2,10 +2,9 @@ from flask import Flask, render_template, redirect, url_for, request
 
 
 app = Flask(__name__)
-@app.route('/home',methods=["GET"])
-def loginpage():
-	return ("welcome to the home page")
-
+@app.route('/web',methods=['GET'])
+def webpage():
+        return render_template('page.html')
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
@@ -13,7 +12,11 @@ def login():
         if request.form['username'] != 'Pratyush' or request.form['password'] != '12345':
             error = 'Invalid Credentials. Please try again.'
         else:
-            return redirect(url_for('loginpage'))
+            return redirect(url_for('webpage'))
+
     return render_template('login.html', error=error)
+
+
 if __name__=="__main__":
-	app.run(host="0.0.0.0",port=8080)
+        app.run(host="0.0.0.0",port=8080)
+
