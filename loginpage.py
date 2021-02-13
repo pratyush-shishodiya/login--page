dec91c7ffe94db655e752e6707e6cd983d840bf6
pratyush-shishodiya <76863772+pratyush-shishodiya@users.noreply.github.com>
2021-02-14 00:46:42 +0530

    Update loginpage.py

--- loginpage.py
+++ loginpage.py
@@ -1,22 +1,31 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask,render_template,redirect,url_for,request,g,session
+import os
 
+app=Flask(__name__)
+app.secret_key=os.urandom(24)
 
-app = Flask(__name__)
-@app.route('/web',methods=['GET'])
+@app.route("/",methods=["GET","POST"])
+def index():
+    if request.method=="POST":
+        session.pop("user",None)
+        if request.form['password']=='password':
+            session["user"]=request.form['username']
+            return redirect(url_for("webpage"))
+    return render_template("login.html")
+@app.route("/page",methods=["GET"])
 def webpage():
-        return render_template('page.html')
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'Pratyush' or request.form['password'] != '12345':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            return redirect(url_for('webpage'))
+    if g.user:
+        return render_template("home.html",user=session["user"])
+    return redirect(url_for('index'))
+@app.before_request
+def before_request():
+    g.user=None
+    if 'user' in session:
+        g.user=session['user']
+@app.route("/dropsession")
+def dropsession():
+    session.pop("user",None)
+    return render_template("login.html")
 
-    return render_template('login.html', error=error)
-
-
-if __name__=="__main__":
-        app.run(host="0.0.0.0",port=8080)
-
+if __name__=='__main__':
+    app.run(debug=True)
